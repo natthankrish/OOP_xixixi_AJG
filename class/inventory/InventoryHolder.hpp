da4952fc690290bc52edf914f20e02e732d208c0
egijago <16521433@mahasiswa.itb.ac.id>
2023-03-01 09:59:38 +0700

    feat:InventoryHolder

--- class/inventory/InventoryHolder.hpp
+++ class/inventory/InventoryHolder.hpp
@@ -1,13 +1,20 @@
 #ifndef INVENTORYHOLDER_HPP
 #define INVENTORYHOLDER_HPP
 
+#include <string>
 #include <iostream>
+
+using namespace std;
 
 class InventoryHolder {
     private:
+        string type;
         //List<Card> tableCard;
         
     public:
+        void setType(string);
+        string getType();
+
 };
 
 #endif
