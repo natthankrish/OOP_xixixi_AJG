1c64f106e994253baeb222401c576ccb3e5749ac
egijago <16521433@mahasiswa.itb.ac.id>
2023-03-12 15:50:27 +0700

    feat: NumberCard operator !=, operator==, dtor

--- class/Card/NumberCard.cpp
+++ class/Card/NumberCard.cpp
@@ -3,20 +3,32 @@
 #include <string>
 using namespace std;
 
+NumberCard::NumberCard(): number(-1){
+}
+
 NumberCard::NumberCard(int number, string color): number(number), color(color) {
 }
-int NumberCard::getNumber(){
+
+int NumberCard::getNumber() {
     return this->number;
 }
 
-void NumberCard::setNumber(int number){
+void NumberCard::setNumber(int number) {
     this->number = number;
 }
 
-string NumberCard::getColor(){
+string NumberCard::getColor() {
     return this->color;
 }
 
-void NumberCard::setColor(string color){
+void NumberCard::setColor(string color) {
     this->color = color;
+}
+
+bool NumberCard::operator==(const NumberCard& other) {
+    return (this->number == other.number && this->color == other.color);
+}
+
+bool NumberCard::operator!=(const NumberCard& other) {
+    return (!(*this == other));
 }
