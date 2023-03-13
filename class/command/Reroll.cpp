3c32c9e7de72e7d9359297471ae352bf3770cf40
melvinkj <melvinkentj@gmail.com>
2023-03-14 01:36:53 +0700

    feat: add Reroll execute implementation

--- class/command/Reroll.cpp
+++ class/command/Reroll.cpp
@@ -8,11 +8,30 @@ Reroll::Reroll(string type, string name) : AbilityCommand(type, name){
 void Reroll::execute(List<Player>& listPlayer, int playeridx, int& prize, CardsBank& cardsbank, AbilityCardsBank& abilitycardsbank) {
     try{
         doesPlayerHaveAbilityCard(listPlayer, playeridx, "Re-roll");
+        playerCardIsDeactivated(listPlayer, playeridx);
         //execution
+        cout << "Melakukan pembuangan kartu yang sedang dimiliki" << endl;
+        NumberCard card1 = listPlayer.getElement(playeridx-1).getPairOfCards().second;
+        NumberCard card2 = listPlayer.getElement(playeridx-1).getPairOfCards().second;
+
+        listPlayer.getElement(playeridx-1)+cardsbank.getElementAt(0);
+        cardsbank-cardsbank.getElementAt(0);
+        listPlayer.getElement(playeridx-1)+cardsbank.getElementAt(0);
+        cardsbank-cardsbank.getElementAt(0);
+
+        cardsbank+card1;
+        cardsbank+card2;
+
+        cout << "Kamu mendapat 2 kartu baru yaitu:";
+        cout << "1. " << listPlayer.getElement(playeridx-1).getPairOfCards().first.getNumber() << " " << listPlayer.getElement(playeridx-1).getPairOfCards().first.getColor() << endl; 
+        cout << "2. " << listPlayer.getElement(playeridx-1).getPairOfCards().second.getNumber() << " " << listPlayer.getElement(playeridx-1).getPairOfCards().second.getColor() << endl; 
+
 
 
     } catch (PlayerDoesNotHaveCardException e) {
         cout << e.what() << "REROLL." << endl;
+    } catch (PlayerCardIsDeactivatedException e) {
+        cout << e.what() << endl;
     }
 }
 
