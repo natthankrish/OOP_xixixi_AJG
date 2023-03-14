8a0055bcbae632ac46b90537ae9bf2108b7805b7
melvinkj <melvinkentj@gmail.com>
2023-03-14 10:24:05 +0700

    feat: reverse execute implementation not done

--- class/command/Reverse.cpp
+++ class/command/Reverse.cpp
@@ -8,10 +8,24 @@ Reverse::Reverse(string type, string name) : AbilityCommand(type, name){
 void Reverse::execute(List<Player>& listPlayer, int playeridx, int& prize, CardsBank& cardsbank, AbilityCardsBank& abilitycardsbank) {
     try{
         doesPlayerHaveAbilityCard(listPlayer, playeridx, "Reverse");
+        playerCardIsDeactivated(listPlayer, playeridx);
         //execution
+        cout << listPlayer.getElement(playeridx-1).getName()<< " melakukan reverse!" << endl;
+        cout << "(sisa) urutan eksekusi giliran ini: ";
+        playeridx = listPlayer.getSize() - playeridx + 1;
+
+        // belum selesai
+        int i = playeridx;
+        for(i ; i < listPlayer.getSize(); i++) {
+            cout << listPlayer.getElement(i).getName() << " ";
+        }
+        cout << endl;
+
         
     } catch (PlayerDoesNotHaveCardException e) {
         cout << e.what() << "REVERSE." << endl;
+    } catch (PlayerCardIsDeactivatedException e) {
+        cout << e.what() << endl;
     }
 }
 bool Reverse::continueToNextPlayer(List<Player>& listPlayer, int playeridx, AbilityCardsBank& abilitycardsbank){
