04091588cef233742a117b89c31c8c986e3c7425
melvinkj <melvinkentj@gmail.com>
2023-03-14 14:53:43 +0700

    fix: Abilityless execute procedure

--- class/command/Abilityless.cpp
+++ class/command/Abilityless.cpp
@@ -13,15 +13,22 @@ void Abilityless::execute(TableCard& tablecard, List<Player>& listPlayer, int pl
         //execution
         cout << listPlayer.getElement(playeridx-1).getName()<< " akan mematikan kartu ability lawan!" << endl;
         cout << "Silahkan pilih pemain yang kartu abilitynya ingin dimatikan: " << endl;
-        
-        for(int i = 1 ; i <= listPlayer.getNeff(); i++){
-            cout << i << ". " << listPlayer.getElement(i).getName() << endl;
+        int j = 1;
+        for(int i = 0; i < listPlayer.getNeff(); i++){
+            if (i != playeridx - 1) {
+                cout << j << ". " << listPlayer.getElement(i).getName() << endl;
+                j++;
+            }
         }
-        cin >> choice;
-
-        playerCardIsUsed(listPlayer, choice);
+        cout << "Pilih angka: ";
+        cin >> choice; 
+        
+        if(choice >= playeridx) {
+            choice++;
+        }
+        playerCardIsUsed(listPlayer, choice-1);
         listPlayer[choice-1].setAbilityStatus(false);
-        cout << "Kartu ability " << listPlayer.getElement(choice).getName() << " telah dimatikan." << endl;
+        cout << "Kartu ability " << listPlayer.getElement(choice-1).getName() << " telah dimatikan." << endl;
     } catch (PlayerDoesNotHaveCardException e) {
         cout << e.what() << "ABILITYLESS." << endl;
         cout << "Silahkan lakukan perintah lain." << endl;
