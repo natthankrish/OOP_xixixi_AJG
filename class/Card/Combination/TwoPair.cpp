6f8b7488339b235d69459589656c0ab503ea1c0f
egijago <16521433@mahasiswa.itb.ac.id>
2023-03-15 09:36:55 +0700

    refactor: handling more cases

--- class/Card/Combination/TwoPair.cpp
+++ class/Card/Combination/TwoPair.cpp
@@ -3,63 +3,80 @@
 
 
 
-double TwoPair::getValue(vector<NumberCard> sortedCards, LookUpTable table) {
-    double bestValue = 0;   
-    for (int i = 0; i < sortedCards.size() - 3; i++) {
-        bool isPair1 = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
-        if (!isPair1) 
-            continue;
-        
-        for (int j = i + 2; j < sortedCards.size() - 1; j++) {
-            bool isPair2 = sortedCards[j + 1].getNumber() == sortedCards[j].getNumber();
-            if (!isPair2)
-                continue;
+double TwoPair::getValue(vector<NumberCard> _sortedCards, LookUpTable table) {
+    vector<NumberCard> sortedCards (_sortedCards);
 
-            double currentValue = 0;
-            for (int k = 0; k < 2; k++) {
-                currentValue += table.getValue(sortedCards[i + k]) / pow(MAX_VALUE, 2 * k);
-                currentValue += table.getValue(sortedCards[j + k]) / pow(MAX_VALUE, 2 * k + 1);
-            }
-
-            currentValue += table.getValue(sortedCards.back()) / pow(MAX_VALUE, 5);
-
-            bestValue = (currentValue > bestValue) ? currentValue : bestValue;
-        } 
-
-    }
-    return MAX_VALUE*2 + bestValue;
-}
-
-vector<NumberCard> TwoPair::getCombo(vector<NumberCard> sortedCards, LookUpTable table) {
-    double bestValue = 0;
-    vector<NumberCard> bestCombo;
-    
-    for (int i = 0; i < sortedCards.size() - 3; i++) {
-        bool isPair1 = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
-        if (!isPair1) 
-            continue;
-
-        for (int j = i + 2; j < sortedCards.size() - 1; j++) {
-            bool isPair2 = sortedCards[j + 1].getNumber() == sortedCards[j].getNumber();
-            if (!isPair2)
-                continue;
-            
-            double currentValue = 0;
-            for (int k = 0; k < 2; k++) {
-                currentValue += table.getValue(sortedCards[i + k]) / (pow(MAX_VALUE, -1 + k));
-                currentValue += table.getValue(sortedCards[j + k]) / (pow(MAX_VALUE, -1 + k));
-            }
-            currentValue += table.getValue(sortedCards[sortedCards.size() - 1]) /MAX_VALUE;
-
-            if (currentValue > bestValue) {
-                bestValue = currentValue;
-                vector<NumberCard> tempCombo = {sortedCards[i], sortedCards[i+1], sortedCards[j], sortedCards[j+1], sortedCards[sortedCards.size()-1]};
-                bestCombo = tempCombo;
-            }
+    int bestPairIndex = -1;
+    for (int i = 0; i < sortedCards.size() - 1; i++) {
+        bool isPair = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
+        if (isPair) {
+            bestPairIndex = i; 
         }
     }
 
-    
+    if (bestPairIndex == -1)
+        return 0;
 
-    return bestCombo;
+    vector<NumberCard> bestPair (sortedCards.begin() + bestPairIndex, sortedCards.begin() + bestPairIndex + 2);
+    sortedCards.erase(sortedCards.begin() + bestPairIndex, sortedCards.begin() + bestPairIndex + 2);
+
+    int secondBestPairIndex = -1;
+    for (int i = 0; i < sortedCards.size() - 1; i++) {
+        bool isPair = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
+        if (isPair) {
+            secondBestPairIndex = i;
+        }
+    }
+
+    if (secondBestPairIndex == -1)
+        return 0;
+
+    vector<NumberCard> secondBestPair (sortedCards.begin() + secondBestPairIndex, sortedCards.begin() + secondBestPairIndex + 2);
+
+    vector<NumberCard> bestTwoPair (secondBestPair);
+    bestTwoPair.insert(bestTwoPair.end(), bestPair.begin(), bestPair.end());
+
+    double bestValue = 0;
+    for (int j = 0; j < 4; j++) {
+        bestValue += table.getValue(bestTwoPair[j]) * (pow(MAX_VALUE, -4 + j));
+    }
+
+    return MAX_VALUE*2 + bestValue;
+}
+
+vector<NumberCard> TwoPair::getCombo(vector<NumberCard> _sortedCards, LookUpTable table) {
+    vector<NumberCard> sortedCards (_sortedCards);
+    double bestValue = 0;
+    
+    int bestPairIndex = -1;
+    for (int i = 0; i < sortedCards.size() - 1; i++) {
+        bool isPair = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
+        if (isPair) {
+            bestPairIndex = i; 
+        }
+    }
+
+    if (bestPairIndex == -1)
+        return vector<NumberCard>();
+
+    vector<NumberCard> bestPair (sortedCards.begin() + bestPairIndex, sortedCards.begin() + bestPairIndex + 2);
+    sortedCards.erase(sortedCards.begin() + bestPairIndex, sortedCards.begin() + bestPairIndex + 2);
+
+    int secondBestPairIndex = -1;
+    for (int i = 0; i < sortedCards.size() - 1; i++) {
+        bool isPair = sortedCards[i + 1].getNumber() == sortedCards[i].getNumber();
+        if (isPair) {
+            secondBestPairIndex = i;
+        }
+    }
+
+    if (secondBestPairIndex == -1)
+        return vector<NumberCard>();
+
+    vector<NumberCard> secondBestPair (sortedCards.begin() + secondBestPairIndex, sortedCards.begin() + secondBestPairIndex + 2);
+
+    vector<NumberCard> bestTwoPair (secondBestPair);
+    bestTwoPair.insert(bestTwoPair.end(), bestPair.begin(), bestPair.end());
+
+    return bestTwoPair;
 }
