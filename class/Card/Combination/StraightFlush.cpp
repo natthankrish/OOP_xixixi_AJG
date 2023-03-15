a4c57d5fb926a42df33045215d5f6786c4b1d774
egijago <16521433@mahasiswa.itb.ac.id>
2023-03-15 09:36:42 +0700

    refactor: handling more cases

--- class/Card/Combination/StraightFlush.cpp
+++ class/Card/Combination/StraightFlush.cpp
@@ -2,53 +2,41 @@
 #include <cmath>
 
 double StraightFlush::getValue(vector<NumberCard> sortedCards, LookUpTable table){
-    double bestValue = 0;
-    for (int i = 0 ; i <sortedCards.size() -4 ; i++){
-        bool isStraightFlush =  sortedCards[i+1].getNumber() == sortedCards[i].getNumber() +1 &&
-                                sortedCards[i+2].getNumber() == sortedCards[i].getNumber() +2 &&
-                                sortedCards[i+3].getNumber() == sortedCards[i].getNumber() +3 &&
-                                sortedCards[i+4].getNumber() == sortedCards[i].getNumber() +4 &&
-                                sortedCards[i+1].getColor() == sortedCards[i].getColor()  && 
-                                sortedCards[i+2].getColor() == sortedCards[i].getColor()  &&             
-                                sortedCards[i+3].getColor() == sortedCards[i].getColor()  && 
-                                sortedCards[i+4].getColor() == sortedCards[i].getColor()  ; 
-        if (!isStraightFlush)
-            continue;
-        double currentValue = 0;
-        for (int j =0; j < 5; j++) {
-            currentValue += table.getValue(sortedCards[i + j]) / (pow(MAX_VALUE, -4 + j));
+    for (int i = sortedCards.size() - 1; i >= 4; i--) {
+        bool isStraightFlush =  sortedCards[i-1].getNumber() == sortedCards[i].getNumber() - 1 &&
+                                sortedCards[i-2].getNumber() == sortedCards[i].getNumber() - 2 &&
+                                sortedCards[i-3].getNumber() == sortedCards[i].getNumber() - 3 &&
+                                sortedCards[i-4].getNumber() == sortedCards[i].getNumber() - 4 &&
+                                sortedCards[i-1].getColor() == sortedCards[i].getColor()  && 
+                                sortedCards[i-2].getColor() == sortedCards[i].getColor()  &&             
+                                sortedCards[i-3].getColor() == sortedCards[i].getColor()  && 
+                                sortedCards[i-4].getColor() == sortedCards[i].getColor()  ; 
+        if (isStraightFlush) {
+            vector<NumberCard> bestCombo = vector<NumberCard> (sortedCards.begin() + i - 4, sortedCards.begin() + i + 1);
+            double bestValue;
+            for (int j = 0; j < 5; j++) {
+                bestValue += table.getValue(bestCombo[j]) * (pow(MAX_VALUE, -5 + j));
+            }
+            return MAX_VALUE*8 + bestValue;
         }
-        bestValue = (currentValue > bestValue) ? currentValue : bestValue;
-    }
-
-    return bestValue;                                        
-
+    }                                     
+    return 0;
 }
 vector<NumberCard> StraightFlush::getCombo(vector<NumberCard> sortedCards, LookUpTable table){
-    double bestValue=0 ;
-    vector<NumberCard> bestCombo;
-
-    for (int i = 0 ; i <sortedCards.size() -4 ; i++){
-        bool isStraightFlush =  sortedCards[i+1].getNumber() == sortedCards[i].getNumber() +1 &&
-                                sortedCards[i+2].getNumber() == sortedCards[i].getNumber() +2 &&
-                                sortedCards[i+3].getNumber() == sortedCards[i].getNumber() +3 &&
-                                sortedCards[i+4].getNumber() == sortedCards[i].getNumber() +4 &&
-                                sortedCards[i+1].getColor() == sortedCards[i].getColor()  && 
-                                sortedCards[i+2].getColor() == sortedCards[i].getColor()  &&             
-                                sortedCards[i+3].getColor() == sortedCards[i].getColor()  && 
-                                sortedCards[i+4].getColor() == sortedCards[i].getColor()  ; 
-        if (!isStraightFlush)
-            continue;
-        double currentValue = 0;
-        for (int j =0; j < 5; j++){
-            currentValue += table.getValue(sortedCards[i + j]) / (pow(MAX_VALUE, -4 + j));
-        }
-        if (currentValue>bestValue){
-            bestValue=currentValue;
-            auto first = sortedCards.begin() + i;
-            auto end = sortedCards.begin() + i + 5;
-            bestCombo = vector<NumberCard>(first, end);
+    for (int i = sortedCards.size() - 1; i >= 4; i--) {
+        bool isStraightFlush =  sortedCards[i-1].getNumber() == sortedCards[i].getNumber() - 1 &&
+                                sortedCards[i-2].getNumber() == sortedCards[i].getNumber() - 2 &&
+                                sortedCards[i-3].getNumber() == sortedCards[i].getNumber() - 3 &&
+                                sortedCards[i-4].getNumber() == sortedCards[i].getNumber() - 4 &&
+                                sortedCards[i-1].getColor() == sortedCards[i].getColor()  && 
+                                sortedCards[i-2].getColor() == sortedCards[i].getColor()  &&             
+                                sortedCards[i-3].getColor() == sortedCards[i].getColor()  && 
+                                sortedCards[i-4].getColor() == sortedCards[i].getColor()  ; 
+        if (isStraightFlush) {
+            vector<NumberCard> bestCombo = vector<NumberCard> (sortedCards.begin() + i - 4, sortedCards.begin() + i + 1);
+            return bestCombo;
         }
     }
-    return bestCombo;
+    
+    return vector<NumberCard>();
 }
